ca5d0e24f692f8c9047ef5bf32c9befa0a28c4f7
pujansharma <sharmapujan209@gmail.com>
2024-05-28 11:40:29 +0530

    project added

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.get("/", (req, res)=>{
+  res.send("Wellcome to dashboard")
+})
 // Public routes
 app.use('/auth', authRoutes);
 
